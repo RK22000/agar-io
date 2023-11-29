89341896853e45797ef4000c3f247b3d35b20482
Rahul <rahukand@gmail.com>
2023-11-28 23:29:46 -0800

    Wrote code to look in a direction.  - snapshot.ipynb  - pov.mp4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import os
 import models.random_model as model
+import sys
 
 #====================================================
 # Find Play Button on Agar IO start page
@@ -13,6 +14,9 @@ while True:
         break
     print("Go to https://agar.io/")
     time.sleep(1)
+pyg.press('f11')
+time.sleep(1)
+box = pyg.locateOnScreen('pics\\play_button.png')
 print(f"located play button at {box}")
 pos = (box.left+box.width/2, box.top+box.height/2)
 pyg.moveTo(*pos)
@@ -25,7 +29,8 @@ pyg.moveTo(*pos)
 # Start and play Agar IO Game
 #----------------------------------------------------
 pyg.leftClick()
-dataset_size = int(os.listdir('data_imitation')[-1].split('-')[0]) 
+
+dataset_size = int(os.listdir('data_imitation')[-1].split('-')[0]) if os.listdir('data_imitation') else 0
 while (box:=pyg.locateOnScreen("pics\\cont_button.png")) is None:
     pos = pyg.position()
     im = pyg.screenshot()
@@ -50,6 +55,9 @@ while (box:=pyg.locateOnScreen("pics\\cont_button.png")) is None:
     print(f"System playing to {pos}")
     pyg.moveTo(*pos, 0.3)
 
+pyg.press('f11')
+time.sleep(1)
+box=pyg.locateOnScreen("pics\\cont_button.png")
 pos = (box.left+box.width/2, box.top+box.height/2)
 pyg.moveTo(*pos)
 
