e56e917a5a55cbb46d83c3e5f4701e34dde8e0d3
Rahul <rahukand@gmail.com>
2024-05-10 00:59:00 -0700

    Made the game bot commandline callable

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ I'm looking to apply this [paper](https://arxiv.org/abs/1704.05588) to a bot tha
 ## Step 1, Build the dataset
 
 [X] Create a script that plays Agar io by moving in a straight line.
+
 [X] Train a model to play agar io
 
 ## Instrutions to run agar.io model
@@ -18,4 +19,10 @@ conda env create -p <Location of new environment> -f env.yml
 conda env create -n <Name of new environment> -f env.yml
 ```
 
-3. Run the first 3 cells in [main2.ipynb](main2.ipynb)
+3. Run the main2.py python script.
+
+```sh
+python main2.py # Run the game bot for one round
+python main2.py -r n # Run the game bot for n rounds
+python main2.py -h # Show help
+```
--- main2.py
+++ main2.py
@@ -16,14 +16,14 @@ def prep_for_round():
         count+=1
         if count == 10: pag.hotkey('ctrl','r')
         if count == 20: return False
-        box = pag.locateOnScreen('pics\\play_button.png')
+        box = pag.locateOnScreen('pics\\play.png', confidence=0.9)
         if box is not None:
             break
         print("Go to https://agar.io/")
         time.sleep(1)
     pag.press('f11')
     time.sleep(1)
-    box = pag.locateOnScreen('pics\\play_button.png')
+    box = pag.locateOnScreen('pics\\play.png', confidence=0.8)
     if box is None: 
         pag.press('f11')
         raise Exception("Could not find play button")
@@ -35,8 +35,8 @@ def prep_for_round():
 def run_round(model, cycle_time=0.05, before_act=lambda img:None, after_act=lambda img:None, timeout=float('inf')):
     pag.leftClick() # Click the start button
     time.sleep(1)
-    cont_pic = os.path.join("pics", "cont_button.png")
-    play_pic = os.path.join("pics", "play_button.png")
+    cont_pic = os.path.join("pics", "cont.png")
+    play_pic = os.path.join("pics", "play.png")
     game_over = lambda img: pag.locate(cont_pic, img) is not None or pag.locate(play_pic, img) is not None
     run_round.running = True
     def _game_over():
@@ -70,10 +70,10 @@ def run_round(model, cycle_time=0.05, before_act=lambda img:None, after_act=lamb
 
     pag.press('f11')
     time.sleep(1)
-    box=pag.locateOnScreen("pics\\cont_button.png")
+    box=pag.locateOnScreen("pics\\cont.png", confidence=0.8)
     click=True
     if box is None:
-        box = pag.locateOnScreen('pics\\play_button.png')
+        box = pag.locateOnScreen('pics\\play.png', confidence=0.8)
         click=False
     if box is None:
         raise Exception("Could not find continue button nor play button")
@@ -95,3 +95,13 @@ def act(img: Image, model):
     # pred *= min(pag.size())
     # pred += [i/2 for i in pag.size()]
 
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser("python main2.py", description="This script runs the agar io bot.")
+    parser.add_argument("-r", type=int, default=1, help="How many rounds of agar io to play")
+    args = parser.parse_args()
+    from models import tflite_ensemble
+    for round in range(args.r):
+        print(f"Round: {round}")
+        if prep_for_round():
+            run_round(tflite_ensemble)
